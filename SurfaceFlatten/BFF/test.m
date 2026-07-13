93a5b89ff5b153d0049dcc0c941b98b0f7df3086
ChangQingScq <107191902+ChangQingScq@users.noreply.github.com>
2026-07-13 18:12:03 +0800

    1
    Removed duplicate line that fixed a small bug.

--- SurfaceFlatten/BFF/test.m
+++ SurfaceFlatten/BFF/test.m
@@ -42,7 +42,6 @@ u = zeros(length(B), 1);
 u= u+log(3);
 pt2 = BFFScale(v, f, u);% see https://peng00bo00.github.io/blog/2022/GAMES301-BFF/ ---Fixed a small bug
 
-Fixed a small bug
 %myPlot(pt,'.');
 
 %[Umin,Umax,Cmin,Cmax,Cmean,Cgauss,Normal] = compute_curvature(v,f);
